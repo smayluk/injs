4ad2bb8e12d9e694f952fc377ea278d8ccaab6fe
smayluk <smayluk@gmail.com>
2018-06-27 17:28:21 +0300

    material ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,100 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+import yellow from '@material-ui/core/colors/yellow';
+import lightBlue from '@material-ui/core/colors/lightBlue';
 
-class App extends Component {
+const theme = createMuiTheme({
+  palette: {
+    primary: yellow,
+    secondary: lightBlue,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
+
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  appFrame: {
+    zIndex: 1,
+    overflow: 'hidden',
+    position: 'relative',
+    display: 'flex',
+    width: '100%',
+    height: '100%',
+  },
+  appBar: {
+    width: `calc(100% - ${drawerWidth}px)`,
+  },
+  'appBar-left': {
+    marginLeft: drawerWidth,
+  },
+  'appBar-right': {
+    marginRight: drawerWidth,
+  },
+  drawerPaper: {
+    position: 'relative',
+    width: drawerWidth,
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing.unit * 3,
+  },
+});
+
+class PermanentDrawer extends React.Component {
+
   render() {
+    const { classes } = this.props;
+
+
+
+
+
     return (
-      <div>
-        inJS
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <div className={classes.appFrame}>
+          <AppBar className={classes.appBar}
+            position="absolute"
+          >
+            <Toolbar>
+              <Typography variant="title" color="inherit" noWrap>
+                in Js
+              </Typography>
+            </Toolbar>
+          </AppBar>
+          <Drawer
+            variant="permanent"
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <div className={classes.toolbar} />
+            <Divider />
+            <Divider />
+          </Drawer>
+          <main className={classes.content}>
+            <div className={classes.toolbar} />
+            <Typography>{'Hi'}</Typography>
+          </main>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+
+export default withStyles(styles)(PermanentDrawer);
